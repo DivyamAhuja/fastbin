06d38a5e2fc0946520f7ca7938b4faf7322748b0
Divyam Ahuja <ahujadiyam@gmail.com>
2024-10-26 09:13:56 +0530

    Add support for loading .env files

---
+++ internal/pkg/env/env.go
@@ -0,0 +1,28 @@
+package env
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	env := os.Getenv("FASTBIN_ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	godotenv.Load(".env." + env + ".local")
+	if env != "test" {
+		godotenv.Load(".env.local")
+	}
+	godotenv.Load(".env." + env)
+	godotenv.Load()
+}
+
+func GetEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
